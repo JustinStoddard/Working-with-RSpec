4a521bf7a0e345c440f251989ddd7d8b662cf9ac
Justin Stoddard <Stoddard.j.t@gmail.com>
2018-04-05 13:13:15 -0600

    new files

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,28 @@
 class Car < ApplicationRecord
+  validates :make, uniqueness :make, presence :true
+
+  def self.by_model
+    order(:model)
+  end
+
+  def self.by_price(direction = :asc)
+      order(price: direction)
+  end
+
+  def honk
+    'BEEP BEEP'
+  end
+
+  def info
+    {
+      make: self.make
+      model: self.model
+      price: self.price
+    }
+  end
+
+  def paint(color)
+    self.update(color: color)
+  end
+ 
 end
